2d01cb5bb37d32bb406412b63f6da96e4826fd92
JES <jamutexj@gmail.com>
2025-08-06 08:11:41 +1000

    add initialize for IAVSRegistar

--- src/ServiceManager.sol
+++ src/ServiceManager.sol
@@ -6,7 +6,7 @@ import {ServiceManagerBase, ISlashingRegistryCoordinator} from "@eigenlayer-midd
 import {IAVSDirectory} from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
 import {IRewardsCoordinator} from "eigenlayer-contracts/src/contracts/interfaces/IRewardsCoordinator.sol";
 import {IPermissionController} from "eigenlayer-contracts/src/contracts/interfaces/IPermissionController.sol";
-import {IAVSRegistrar} from "@eigenlayer/contracts/interfaces/IAVSRegistrar.sol";
+import {IAVSRegistrar} from "eigenlayer-contracts/src/contracts/interfaces/IAVSRegistrar.sol";
 import {IAllocationManager} from "eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 import {IStakeRegistry} from "@eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
 
@@ -36,5 +36,35 @@ contract ServiceManager is ServiceManagerBase {
         )
     {
         _disableInitializers();
+    }
+
+    function initialize(
+        address initialOwner_,
+        address rewardsInitiator_,
+        string calldata metadataURI_
+    ) external initializer {
+        __ServiceManagerBase_init(initialOwner_, rewardsInitiator_);
+
+        _allocationManager.updateAVSMetadataURI(address(this), metadataURI_);
+        _allocationManager.setAVSRegistrar(address(this), IAVSRegistrar(address(_registryCoordinator)));
+        _permissionController.addPendingAdmin(address(this), initialOwner_);
+        _permissionController.setAppointee({
+            account: address(this),
+            appointee: address(_registryCoordinator),
+            target: address(_allocationManager),
+            selector: _allocationManager.createOperatorSets.selector
+        });
+        _permissionController.setAppointee({
+            account: address(this),
+            appointee: address(this),
+            target: address(_allocationManager),
+            selector: _allocationManager.deregisterFromOperatorSets.selector
+        });
+        _permissionController.setAppointee({
+            account: address(this),
+            appointee: address(this),
+            target: address(_allocationManager),
+            selector: _allocationManager.slashOperator.selector
+        });
     }
 }
